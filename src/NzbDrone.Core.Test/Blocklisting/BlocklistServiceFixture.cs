99130f8f4d6e1d862390197f37193fc8a4a8fd5d
iuliandita <iulian.dita@gmail.com>
2026-09-15 00:46:21 +0200

    fix(blocklist): treat a republished usenet release as the same NZB
    Indexers can report the same post again with a different published
    timestamp, which made the automatic failed-download search grab the
    same bad release forever. Match on the title first; the repository
    already narrows candidates to this author.
    
    Closes #98

--- src/NzbDrone.Core.Test/Blocklisting/BlocklistServiceFixture.cs
+++ src/NzbDrone.Core.Test/Blocklisting/BlocklistServiceFixture.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.Download;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Parser.Model;
 using NzbDrone.Core.Qualities;
 using NzbDrone.Core.Test.Framework;
 
@@ -53,6 +56,66 @@ namespace NzbDrone.Core.Test.Blocklisting
 
             Mocker.GetMock<IBlocklistRepository>()
                 .Verify(v => v.Insert(It.Is<Blocklist>(b => b.BookIds == _event.BookIds)), Times.Once());
+        }
+
+        [Test]
+        public void should_block_a_usenet_release_when_only_the_published_timestamp_changed()
+        {
+            var release = new ReleaseInfo
+            {
+                Title = "author.name.book.title",
+                DownloadProtocol = DownloadProtocol.Usenet,
+                Indexer = "nzbs.org",
+                PublishDate = new DateTime(2022, 3, 31, 10, 18, 0),
+                Size = 361219000
+            };
+
+            Mocker.GetMock<IBlocklistRepository>()
+                .Setup(v => v.BlocklistedByTitle(12345, release.Title))
+                .Returns(new List<Blocklist>
+                {
+                    new Blocklist
+                    {
+                        AuthorId = 12345,
+                        SourceTitle = release.Title,
+                        Protocol = DownloadProtocol.Usenet,
+                        Indexer = null,
+                        PublishedDate = new DateTime(2022, 4, 30, 10, 26, 30),
+                        Size = 361219000
+                    }
+                });
+
+            Subject.Blocklisted(12345, release).Should().BeTrue();
+        }
+
+        [Test]
+        public void should_not_block_a_usenet_release_with_a_different_title()
+        {
+            var release = new ReleaseInfo
+            {
+                Title = "author.name.book.title",
+                DownloadProtocol = DownloadProtocol.Usenet,
+                Indexer = "nzbs.org",
+                PublishDate = new DateTime(2022, 3, 31, 10, 18, 0),
+                Size = 361219000
+            };
+
+            Mocker.GetMock<IBlocklistRepository>()
+                .Setup(v => v.BlocklistedByTitle(12345, release.Title))
+                .Returns(new List<Blocklist>
+                {
+                    new Blocklist
+                    {
+                        AuthorId = 12345,
+                        SourceTitle = "a.different.release",
+                        Protocol = DownloadProtocol.Usenet,
+                        Indexer = null,
+                        PublishedDate = new DateTime(2022, 4, 30, 10, 26, 30),
+                        Size = 361219000
+                    }
+                });
+
+            Subject.Blocklisted(12345, release).Should().BeFalse();
         }
     }
 }
