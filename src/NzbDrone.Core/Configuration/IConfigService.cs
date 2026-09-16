13ed34d364db349a15746e08143c1a937a625c69
iuliandita <iulian.dita@gmail.com>
2026-09-16 13:58:38 +0200

    feat(metadata): secondary source with failover and health check (#105) (#131)

--- src/NzbDrone.Core/Configuration/IConfigService.cs
+++ src/NzbDrone.Core/Configuration/IConfigService.cs
@@ -68,6 +68,7 @@ namespace NzbDrone.Core.Configuration
 
         //Metadata
         string MetadataSource { get; set; }
+        string MetadataSourceSecondary { get; set; }
         WriteAudioTagsType WriteAudioTags { get; set; }
         bool ScrubAudioTags { get; set; }
         WriteBookTagsType WriteBookTags { get; set; }
